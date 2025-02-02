53c532054439dd7ed8def09fc579470fce9383f8
422949798 <422949798@qq.com>
2025-02-02 18:11:12 +0800

    feat(用户管理)

--- backend/router/user_router.go
+++ backend/router/user_router.go
@@ -9,10 +9,15 @@ import (
 
 func (router RouterGroup) UserRouter(userRouterGroup *gin.RouterGroup) {
 	app := api.ApiGroupApp.UserApi
-
+	userRouterGroup.PUT("change_password/:id", app.ChangePassword)
 	userRouterGroup.POST("login", app.UserLoginView)
 	userRouterGroup.POST("refresh", app.RefreshTokenView)
 	userRouterGroup.Use(middleware.JwtUser())
 	userRouterGroup.GET("permission_info/:id", app.UserPermissionInfoView)
 	userRouterGroup.PUT("assign_permission", app.AssignPermission)
+	userRouterGroup.POST("user_create", app.UserCreate)
+	userRouterGroup.PUT("user_disable/:id", app.UserDisable)
+	userRouterGroup.DELETE("user_delete/:id", app.UserDelete)
+	userRouterGroup.GET("user_list", app.UserList)
+
 }
