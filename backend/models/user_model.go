a25426b6f62351bd32e89a07b1e54a93feb38ac4
赵嘉琦 <149140262+422949798@users.noreply.github.com>
2025-02-26 17:54:06 +0800

    修改创建用户接口请求体；修改权限主机列表接口；修改用户列表接口返回字段 (#42)
    * fix(用户管理):用户权限主机列表结构优化
    
    * fix(用户管理):分配权限接口请求体结构优化
    
    * fix(用户管理):分配权限接口请求体修改,用户列表接口响应字段修改

--- backend/models/user_model.go
+++ backend/models/user_model.go
@@ -3,14 +3,19 @@ package models
 type UserModel struct {
 	MODEL
 
-	Username  string       `gorm:"column:username;size:36;comment:用户名" json:"username"`                                 // 用户名
-	Password  string       `gorm:"size:128;comment:密码" json:"-"`                                                        // 密码
-	Role      string       `gorm:"size:128;default:用户;comment:权限，1系统管理员，用户" json:"role"`                                // 权限
-	IsInit    bool         `gorm:"size:4;default:0;comment:是否初始化，0未初始化，1已初始化" json:"isInit"`                            // 是否初始化  0 未初始化  1 已初始化
-	IsEnabled bool         `gorm:"default:1;comment:用户是否启用，1启用，0禁用" json:"isEnabled"`                                   // 用户是否启用，默认启用
-	Email     string       `gorm:"size:128;comment:邮箱" json:"email"`                                                    // 邮箱
-	Hosts     []HostModel  `gorm:"many2many:host_permissions;joinForeignKey:UserId;joinReferences:HostId" json:"hosts"` // 关联的主机列表
-	Labels    []LabelModel `gorm:"many2many:user_labels;joinForeignKey:UserID;joinReferences:LabelID" json:"labels"`    // 关联的标签列表
-
+	Username    string         `gorm:"column:username;size:36;comment:用户名" json:"username"`                             // 用户名
+	Password    string         `gorm:"size:128;comment:密码" json:"-"`                                                    // 密码
+	Role        string         `gorm:"size:128;default:用户;comment:权限:admin|user" json:"role"`                           // 权限
+	IsInit      bool           `gorm:"size:4;default:0;comment:是否初始化，0未初始化，1已初始化" json:"isInit"`                        // 是否初始化  0 未初始化  1 已初始化
+	IsEnabled   bool           `gorm:"default:1;comment:用户是否启用，1启用，0禁用" json:"isEnabled"`                               // 用户是否启用，默认启用
+	Email       string         `gorm:"size:128;comment:邮箱" json:"email"`                                                // 邮箱
+	Hosts       []HostModel    `gorm:"many2many:host_permissions;joinForeignKey:UserId;joinReferences:HostId" json:"-"` // 关联的主机列表
+	Labels      []LabelModel   `gorm:"many2many:user_labels;joinForeignKey:UserID;joinReferences:LabelID" json:"-"`     // 关联的标签列表
+	Permissions UserPermission `json:"permissions" gorm:"-"`
 	//是否启用
+}
+
+type UserPermission struct {
+	HostIds  []uint `json:"hostIds"`
+	LabelIds []uint `json:"labelIds"` // 新增标签ID列表
 }
